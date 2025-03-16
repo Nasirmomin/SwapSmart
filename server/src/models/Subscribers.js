19c3280738b1b0b43133d9fc1acc875132f596bf
shubham <shubham@tecoreng.com>
2025-03-16 15:57:49 +0530

    Server deleted

--- server/src/models/Subscribers.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize, DataTypes } from 'sequelize';
-import {sequelize} from '../config/dbConnection.js';
-
-const Subscriber = sequelize.define('Subscriber', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    email: { type: DataTypes.STRING, unique: true, allowNull: false },
-}, { timestamps: true });
-
-export default Subscriber;
